9c09319fbc64a2324e2657dce7964cacb05146ce
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-01-07 16:47:58 +0200

    Chapter 3 lesson 3 added

---
+++ 03-control-structures/03-comparison-logical-operators/index.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+| Comparison Operators
+| Operator | Description              |
+| -------- | ------------------------ |
+| ==       | Equal to                 |
+| ===      | Identical to             |
+| !=       | Not equal to             |
+| <>       | Not equal to             |
+| !==      | Not identical to         |
+| <        | Less than                |
+| >        | Greater than             |
+| <=       | Less than or equal to    |
+| >=       | Greater than or equal to |
+*/
+$x = 10;
+$y = '10';
+
+// var_dump($x == $y); // true (== tests the value)
+// var_dump($x === $y);
+// var_dump($x != $y);
+// var_dump($x !== $y);
+// var_dump($x <> $y); // same as !=
+// var_dump($x > $y);
+// var_dump($x < $y);
+// var_dump($x <= $y);
+// var_dump($x >= $y);
+
+// $a = 10;
+// $b = 21;
+$a = 11;
+$b = 20;
+// var_dump($a == 10 && $b == 20);
+// var_dump($a == 10 and $b == 20);
+// var_dump($a == 10 || $b == 20);
+// var_dump($a == 10 or $b == 20);
+// var_dump($a == 10 xor $b == 20);
+// var_dump(!($a == 5));
+
+/*
+| Logical Operators
+| Operator | Description            |
+| -------- | ---------------------- |
+| and      | True if both are true  |
+| &&       | True if both are true  |
+| or       | True if either is true |
+| ||       | True if either is true |
+| xor      | True if one is true    |
+| !        | True if it is not true |
+*/
